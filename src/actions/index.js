073a4bff44be517bfba833260d8d8bcc4afdd75a
Amit Kumar Das <amit.inverse@gmail.com>
2023-01-29 17:59:05 +0530

    Making a Request from an Action Creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = async () => {
+    // BAD APPROACH s!!!
+    const response = await jsonPlaceholder.get('/post');
+
     return {
         type: 'FETCH_POSTS',
+        payload: response,
     };
 };
